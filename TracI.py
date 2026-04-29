8b3b5b0b42c5f79a6933806a7f3206c45cf617bc
T123093 <takutaku041220@gmail.com>
2026-04-29 14:10:33 +0900

    TracIのコード(途中)を追加

---
+++ TracI.py
@@ -0,0 +1,8 @@
+import os
+import sys
+
+if 'SUMO_HOME' in os.environ:
+    tools = os.path.join(os.environ['SUMO_HOME'], 'tools')
+    sys.path.append(tools)
+else:
+    raise EnvironmentError("SUMO_HOMEが設定されていません")
